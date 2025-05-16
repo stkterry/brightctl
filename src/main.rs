62a5b20229ce2aaf37b1135dfd2934127e00ee6c
stkterry <31839035+stkterry@users.noreply.github.com>
2025-05-16 14:27:16 -0400

    Completed "Set" command, some code cleanup and simplification
    Add systemd crate, add release profile, add functions: (parse_value, set_brightness), simplify list option code, complete  "Set" command

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
-use std::{fs::{read_to_string, DirEntry}, path::Path};
+
+use std::{
+    ffi::c_void, 
+    fs::{read_to_string, DirEntry}, 
+    path::Path
+};
 
 use clap::{builder::OsStr, Parser, Subcommand};
-use anyhow::{Error, Result};
+use anyhow::{anyhow, Result};
+use systemd::bus::{Bus, BusName, InterfaceName, MemberName, ObjectPath};
 
-#[inline(always)]
-fn percent(top: u32, bottom: u32) -> u8 {
-    (top as f32 / bottom as f32 * 100f32) as u8
+
+
+#[derive(Debug)]
+enum Adj {
+    Abs,
+    Pos,
+    Neg,
+}
+
+struct Value {
+    _adj: Adj,
+    qty: u32,
+    _per: bool,
 }
 
 struct Device<'a> {
@@ -14,7 +30,6 @@ struct Device<'a> {
     brightness: u32,
     max_brightness: u32,
 }
-
 
 impl <'a>Device<'a> {
 
@@ -26,7 +41,7 @@ impl <'a>Device<'a> {
             Brightness: {} ({}%)
             Max Brightness: {}\n",
             id.file_name().to_str().unwrap(), class, 
-            brightness, percent(*brightness, *max_brightness), 
+            brightness, device.percent_brightness(), 
             max_brightness
         );
     }
@@ -37,10 +52,16 @@ impl <'a>Device<'a> {
         println!(
             "{},{},{},{}%,{}",
             id.file_name().to_str().unwrap(), class, 
-            brightness, percent(*brightness, *max_brightness), 
+            brightness, device.percent_brightness(), 
             max_brightness
         );
     }
+
+    #[inline(always)]
+    fn percent_brightness(&self) -> u8 {
+        (self.brightness as f32 / self.max_brightness as f32 * 100f32) as u8
+    }
+    
 }
 
 #[derive(Subcommand, Debug)]
@@ -60,25 +81,26 @@ enum Commands {
 struct Arguments {
 
     /// List devices with available brightness controls.
-    #[arg(short, long, required=false)]
+    #[arg(short, long)]
     list: bool,
 
     /// Produce machine-readable output.
-    #[arg(short, long, required=false)]
+    #[arg(short, long)]
     machine_readable: bool,
 
     /// Specify Device Name
-    #[arg(short, long, required=false)]
+    #[arg(short, long)]
     device: Option<OsStr>,
 
     /// Specify Class Name
-    #[arg(short, long, required=false)]
+    #[arg(short, long)]
     class: Option<OsStr>,
 
     /// Commands
     #[command(subcommand)]
     command: Option<Commands>
 }
+
 
 fn main() {
     let path = Path::new("/sys/class");
@@ -99,21 +121,17 @@ fn main() {
     if args.list {
 
         let cnt: u16 = classes.into_iter().flat_map(|class| {
-            match path.join(class).read_dir() {
-                Ok(dir) => Some((class, dir)),
-                Err(_) => None
-            }
-        }).fold(0u16, |cnt, (class, path)| {
-            path.into_iter()
-                .flat_map(|entry| read_device(class, entry?))
-                .inspect(printfn)
-                .count() as u16 + cnt
-        });
+            let k = path.join(class)
+                .read_dir().ok()?
+                .flat_map(|dev_path| map_device(class, dev_path?))
+                .count() as u16;
+            Some(k)
+        }).sum();
 
         match (cnt == 0, args.class) {
             (false, _)  => {},
             (_, None) => println!("Failed to read any devices."),
-            (_, Some(class)) => println!("Failed to read any devices of class {:?}", class),
+            (_, Some(ref class)) => println!("Failed to read any devices of class '{}'", class.to_str().unwrap()),
         }
 
         return
@@ -126,15 +144,15 @@ fn main() {
             path.join(class)
                 .read_dir().ok()?
                 .flatten()
-                .find(|entry| entry.file_name().as_os_str() == *id)
-                .and_then(|entry| read_device(class, entry).ok())
+                .find(|dev_path| dev_path.file_name().as_os_str() == *id)
+                .and_then(|dev_path| map_device(class, dev_path).ok())
         })
     } else {
         classes.into_iter().find_map(|class| {
             path.join(class)
                 .read_dir().ok()?
                 .flatten()
-                .find_map(|entry| read_device(class, entry).ok())
+                .find_map(|dev_path| map_device(class, dev_path).ok())
         })
     };
 
@@ -145,41 +163,108 @@ fn main() {
             println!("Failed to find a suitable device.");
             return;
         },
-        (Some(id), _) => {
+        (Some(ref id), _) => {
             println!("Device '{}' not found.", id.to_str().unwrap());
             return;
         }
     };
 
-
-    let cmd = match args.command {
-        Some(cmd) => cmd,
-        None => return
-    };
-
-    match cmd {
+    if let Some(cmd) = args.command { match cmd {
         Commands::Info => printfn(&device),
         Commands::Get => println!("{}", device.brightness),
         Commands::Max => println!("{}", device.max_brightness),
-        Commands::Set { value } => {},
-    }
+        Commands::Set { value } => {
+
+            let value = match parse_value(&device, &value) {
+                Ok(v) => v,
+                Err(err) => { println!("{err}"); return }
+            };
+
+            match set_brightness(&device, &value) {
+                Ok(_) => {},
+                Err(err) => { println!("{err}"); return }
+            }
+        },
+    }}
+
+}
+
+#[inline]
+fn parse_value(device: &Device, value: &OsStr) -> Result<Value> {
+
+    let str = value.to_str()
+        .ok_or_else(|| anyhow!("Value is not valid unicode."))?;
+
+    let edx = *&str.len();
+    let (adj, sdx) = match &str[0..1] {
+        "+" => (Adj::Pos, 1),
+        "-" => (Adj::Neg, 1),
+        _ => (Adj::Abs, 0),
+    };
+
+    let (edx, per) = match &str[edx-1..edx] {
+        "%" => (edx - 1, true),
+        _ => (edx, false)
+    };
+
+    let val: u32 = str[sdx..edx].parse()?;
+
+    let mb = device.max_brightness;
+    let b = device.brightness;
+    use Adj::*;
+    let qty = match (per, &adj) {
+        (true,  Abs) => mb.min((val * mb) / 100),
+        (true,  Pos) => mb.min(b + ((val * mb) / 100)),
+        (true,  Neg) => b - ((val * mb) / 100),
+        (false, Abs) => mb.min(val),
+        (false, Pos) => mb.min(b + val),
+        (false, Neg) => b - val,
+    };
+
+    Ok(Value { _adj: adj, qty, _per: per })
 
 }
 
 
 #[inline]
-fn read_device(class: &str, entry: DirEntry) -> Result<Device, Error> {
+fn map_device(class: &str, dev_path: DirEntry) -> Result<Device> {
 
-    let max_brightness = read_to_string(entry.path().join("max_brightness"))?
+    let max_brightness = read_to_string(dev_path.path().join("max_brightness"))?
         .trim().parse::<u32>()?;
     
-    let brightness = read_to_string(entry.path().join("brightness"))?
+    let brightness = read_to_string(dev_path.path().join("brightness"))?
         .trim().parse::<u32>()?;
 
     Ok(Device {
-        id: entry,
+        id: dev_path,
         class,
         brightness,
         max_brightness
     })
+}
+
+
+#[inline]
+fn set_brightness(device: &Device, value: &Value) -> Result<()> {
+
+    let (dest, path, interface, member) = unsafe {(
+        BusName::from_bytes_unchecked(b"org.freedesktop.login1\0"),
+        ObjectPath::from_bytes_unchecked(b"/org/freedesktop/login1/session/auto\0"),
+        InterfaceName::from_bytes_unchecked(b"org.freedesktop.login1.Session\0"),
+        MemberName::from_bytes_unchecked(b"SetBrightness\0"),
+    )};
+    
+    let mut msg = Bus::default_system()?
+        .new_method_call(dest, path, interface, member)?;
+    
+    
+    unsafe { 
+        msg.append_basic_raw(115, device.class.to_owned().as_ptr() as *const c_void)?;
+        msg.append_basic_raw(115, device.id.file_name().to_string_lossy().as_ptr() as *const c_void)?;
+    };
+    msg.append(value.qty)?;
+    
+    let _ = msg.call(0);
+     
+    Ok(())
 }
